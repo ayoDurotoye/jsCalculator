c0c3486a3f21e41701554f43d299d2e7f6201593
ayoDurotoye <ayomikundurotoye@ayoDurotoye-2.local>
2023-05-11 11:40:14 +0100

    calculator

---
+++ intro.js
@@ -0,0 +1,106 @@
+
+
+
+// function toggle(){
+// let value=document.getElementById('color').value
+
+// document.getElementById('display').value=value
+
+
+
+
+// console.log();
+
+
+function toggle(message){
+   
+    document.getElementById('input-display').value += message
+    
+   
+    console.log();
+    
+}
+
+// function toggle2(){
+//     let button =document.getElementById('mybtn2').value
+
+//     document.getElementById('input-display').value +=2 
+
+    
+   
+//     console.log();
+    
+// }
+
+// function toggle3(){
+//     let button =document.getElementById('mybtn2').value
+//     document.getElementById('input-display').value +=3 
+    
+   
+//     console.log();
+    
+// }
+
+// function toggle4(){
+//     document.getElementById('mybtn2').value
+//     document.getElementById('input-display').value +=4
+    
+   
+//     console.log();
+    
+// }
+
+// function toggle5(){
+//     ldocument.getElementById('mybtn2').value
+//     document.getElementById('input-display').value +=5
+    
+//     console.log();
+    
+// }
+
+// function toggle6(){
+//     let button =document.getElementById('mybtn2').value
+//     document.getElementById('input-display').value +=6
+    
+   
+//     console.log();
+    
+// }
+
+// function toggle7(){
+//     let button =document.getElementById('mybtn2').value
+//     document.getElementById('input-display').value +=7
+    
+   
+//     console.log();
+    
+// }
+
+// function toggle8(){
+//     document.getElementById('mybtn2').value
+//     document.getElementById('input-display').value +=8
+    
+   
+//     console.log();
+    
+// }
+
+// function toggle9(){
+//     let button =document.getElementById('mybtn2').value
+//     document.getElementById('input-display').value +=9
+    
+   
+//     console.log();
+    
+// }
+// function toggle0(){
+//     let button =document.getElementById('mybtn2').value
+//     document.getElementById('input-display').value +=10 
+    
+   
+//     console.log();
+    
+// }
+
+
+
